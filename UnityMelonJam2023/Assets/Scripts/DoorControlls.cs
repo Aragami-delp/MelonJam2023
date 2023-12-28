ce4091d19d3353044305b7987cb7eba31dabcf03
Fuchsiano <Fuchsiano04@gmail.com>
2023-12-28 17:56:32 +0100

    doors working
    Kurrrrrrrrwaaaaa

--- UnityMelonJam2023/Assets/Scripts/DoorControlls.cs
+++ UnityMelonJam2023/Assets/Scripts/DoorControlls.cs
@@ -6,15 +6,28 @@ using UnityEngine;
 public class DoorControlls : MonoBehaviour
 {
     [SerializeField]
-    bool proximityOpen;
+    bool startOpen,proximityOpen;
     [SerializeField]
     float proximityRange;
 
-    Vector3 targetPos;
     bool doLerp;
+    bool openDoor;
+    DoorPos leftDoor;
+    DoorPos rightDoor;
+
 
     void Start()
     {
+        Transform leftDoorChild = transform.GetChild(0);
+        leftDoor = new DoorPos(leftDoorChild.position, GetTargetPos(leftDoorChild), leftDoorChild);
+
+        Transform rightDoorChild = transform.GetChild(1);
+        rightDoor = new DoorPos(rightDoorChild.position, GetTargetPos(rightDoorChild), rightDoorChild);
+
+        if (startOpen) 
+        {
+            OpenDoor();
+        }
 
         if (proximityOpen) 
         {
@@ -31,7 +44,6 @@ public class DoorControlls : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        Debug.Log(collision.name);
         OpenDoor();
     }
     private void OnTriggerExit2D(Collider2D collision)
@@ -48,36 +60,80 @@ public class DoorControlls : MonoBehaviour
         }
     }
 
-
+    float time = 0f;
     private void Update()
     {
+        if (Input.GetKeyDown(KeyCode.Space)) 
+        {
+            OpenDoor();
+        }
+        else if (Input.GetKeyDown(KeyCode.Escape)) 
+        {
+            CloseDoor();
+        }
+
         if (!doLerp) return;
 
         foreach (Transform child in transform)
         {
+            time += Time.deltaTime;
 
-            child.transform.position += Vector3.Lerp(child.transform.position,targetPos,0f);
+            if (openDoor)
+            {
+                leftDoor.Child.position = Vector3.Lerp(leftDoor.StartPos, leftDoor.EndPos, time);
+                rightDoor.Child.position = Vector3.Lerp(rightDoor.StartPos, rightDoor.EndPos, time);
+            }
+            else 
+            {
+                leftDoor.Child.position = Vector3.Lerp(leftDoor.EndPos, leftDoor.StartPos, time);
+                rightDoor.Child.position = Vector3.Lerp(rightDoor.EndPos, rightDoor.StartPos, time);
+            }
+
+            if (time >= 1f) 
+            {
+                doLerp = false;
+            }
         }
     }
-
+    [ContextMenu("OpenDoor")]
     public void OpenDoor() 
     {
-        foreach (Transform child in transform)
+        if (leftDoor.Child.position != leftDoor.EndPos && !openDoor && !doLerp) 
         {
-            Debug.Log(child.transform.right * openAmmount.x + child.transform.right * openAmmount.y);
-            child.transform.position += child.transform.right * openAmmount.x + child.transform.right * openAmmount.y;
+            doLerp = true;
+            openDoor = true;
+            time = 0f;
         }
     }
-
+    [ContextMenu("CloseDoor")]
     public void CloseDoor() 
     {
-        foreach (Transform child in transform)
+        if (leftDoor.Child.position != leftDoor.StartPos && openDoor && !doLerp) 
         {
-            child.transform.position += -(child.transform.right) * openAmmount.x + -(child.transform.right) * openAmmount.y;
+            doLerp = true;
+            openDoor = false;
+            time = 0f;
         }
     }
 
+    private Vector3 GetTargetPos(Transform child) 
+    {
+            return child.transform.position + child.transform.right * openAmmount.x + child.transform.right * openAmmount.y;
+    }
 
+    private struct DoorPos 
+    {
+        public Vector3 StartPos;
+        public Vector3 EndPos;
+        public Transform Child;
+
+        public DoorPos(Vector3 startPos, Vector3 endPos,Transform child)
+        {
+            StartPos = startPos;
+            EndPos = endPos;
+            Child = child;
+        }
+    }
 }
 [CustomEditor(typeof(DoorControlls))]
 public class DoorControllsEditor : Editor
