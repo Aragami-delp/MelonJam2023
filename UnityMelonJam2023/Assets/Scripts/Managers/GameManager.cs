f637b30981c128e69cc4e9735800581cbdc73bc4
Aragami_ <Aragami@aragami.com>
2023-12-28 12:36:32 +0100

    Useless manager

---
+++ UnityMelonJam2023/Assets/Scripts/Managers/GameManager.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager Instance { get; private set; }
+    // Start is called before the first frame update
+    private void Awake()
+    {
+        if (Instance != null)
+        {
+            Destroy(this.gameObject);
+            return;
+        }
+        Instance = this;
+    }
+}
