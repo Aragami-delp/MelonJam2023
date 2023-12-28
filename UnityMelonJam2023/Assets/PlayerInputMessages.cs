04f4c73865bf711a2c17c49bb3988bf22baa4529
Aragami_ <Aragami@aragami.com>
2023-12-28 17:09:59 +0100

    added New Input System

---
+++ UnityMelonJam2023/Assets/PlayerInputMessages.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerInputMessages : MonoBehaviour
+{
+    private Movement2D _movement;
+    private void Awake()
+    {
+        _movement = GetComponent<Movement2D>();
+    }
+
+    public void Move(InputAction.CallbackContext context)
+    {
+        _movement.MovePlayerInput = context.ReadValue<Vector2>().normalized;
+    }
+
+    public void Aim(InputAction.CallbackContext context)
+    {
+        if (context.control.device is Mouse)
+        {
+            _movement.AimDirPlayerInput = Utility.GetMousePos().ToVector3() - _movement.transform.position;
+        }
+        else
+        {
+            _movement.AimDirPlayerInput = context.ReadValue<Vector2>().normalized;
+        }
+    }
+}
